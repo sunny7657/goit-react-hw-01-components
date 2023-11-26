cf4e06752b8773a420b0c2ac4ced43a41e916d3d
sunny7657 <ksvetlana466@gmail.com>
2023-11-26 19:41:44 +0100

    upd transactions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import user from '../Data/user.json';
 import data from '../Data/data.json';
 import friends from '../Data/friends.json';
+import transactions from '../Data/transactions.json';
 import { Container } from './Container/Container';
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
@@ -18,6 +20,7 @@ export const App = () => {
       />
       <Statistics data={data} title="Upload stats" />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </Container>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,27 @@
+import './TransactionHistory.css';
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <section className="section transaction">
+      <table className="transaction-table">
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {items.map(({ id, type, amount, currency }) => (
+            <tr key={id}>
+              <td>{type}</td>
+              <td>{amount}</td>
+              <td>{currency}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
+  );
+};
