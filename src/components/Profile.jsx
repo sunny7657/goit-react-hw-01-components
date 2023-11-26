1dec4d7aa2827e1f7aadf911fd01e1d26007c4bc
sunny7657 <ksvetlana466@gmail.com>
2023-11-26 11:49:34 +0100

    upd

---
+++ src/components/Profile.jsx
@@ -0,0 +1,9 @@
+import user from "../user.json";
+
+const Profile = () => {
+//   username={user.username}
+//   tag={user.tag}
+//   location={user.location}
+//   avatar={user.avatar}
+//   stats={user.stats}
+}
